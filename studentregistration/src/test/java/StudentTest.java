51d6cc22bab11dc3b54cfc45a09c88e19382b119
ELOI-CODE1 <izabayoeloi2@gmail.com>
2026-07-19 16:30:37 +0200

    updte test method

--- studentregistration/src/test/java/StudentTest.java
+++ studentregistration/src/test/java/StudentTest.java
@@ -1,3 +1,7 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 import com.example.domain.Gender;
 import com.example.domain.Student;
 import com.example.service.StudentService;
@@ -7,15 +11,18 @@ public class StudentTest {
 
     StudentService service = new StudentService();
 
+
+    @Test
     public void saveStudentTest(){
         Student oneStudent = new Student();
         oneStudent.setId(1);
         oneStudent.setFirstName("Eric");
         oneStudent.setLastName("Mubi");
-        oneStudent.setGender("male");
+        oneStudent.setGender(Gender.MALE);
         oneStudent.setDateOfbirth("12/10/2013");
 
         String result = service.saveStudent(oneStudent);
-        
+        assertEquals("Student saved successfuly", result);
+
     }
 }
